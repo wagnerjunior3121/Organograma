9e1d04edd09ef2fa570955eb806d9be85bfcdd49
wagnerjunior3121 <wagnersantos2003@Outlook.com>
2024-11-25 10:59:11 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000; 
+const PORT = process.env.PORT || 10000; 
 
 app.use(cors()); 
 app.use(express.json()); 
